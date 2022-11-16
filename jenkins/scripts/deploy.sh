5e800ce1ba882a505aef22736ddd216d8644e91e
Don-Whis <2000522@sit.singaporetech.edu.sg>
2022-11-16 14:12:25 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:81 --name my-apache-php-app -v C:\\Users\\chen8\\Documents\\GitHub\\3103lab8-2\\src:/var/www/html php:7.2-apache
+docker run -d -p 8000:8000 --name my-apache-php-app -v C:\\Users\\chen8\\Documents\\GitHub\\3103lab8-2\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
